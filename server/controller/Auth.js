3855cefe145eb21a86fdcd023a95e46920be137d
tanish <tanishsoni0415@gmail.com>
2025-07-21 23:07:40 +0530

    change secure to true

--- server/controller/Auth.js
+++ server/controller/Auth.js
@@ -34,7 +34,7 @@ const LoginUser = async (req, res) => {
           res.cookie("token", token, {
             httpOnly: true,
             secure: true, // set to true if using HTTPS
-            sameSite: "Lax", // or "Strict"
+            sameSite: 'none', // or "Strict"
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
           });
           res.status(201).json(token);
@@ -130,7 +130,7 @@ const createUser = async (req, res) => {
         res.cookie("token", token, {
           httpOnly: true,
           secure: true, // set to true if using HTTPS
-          sameSite: "Lax", // or "Strict"
+          sameSite:  'none', // or "Strict"
           maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
         res.status(201).json({ token });
